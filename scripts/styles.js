e21ff13da507595112a90dadde6cb5f3f223d7c7
Cherkashin Alexander <cherkalexander@gmail.com>
2023-11-12 10:11:40 +0300

    Fix variables.css name in import

--- scripts/styles.js
+++ scripts/styles.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import postcss from 'postcss';
 
-const tokensPath = path.resolve('src/tokens.css');
+const tokensPath = path.resolve('src/variables.css');
 const cssContent = fs.readFileSync(tokensPath, 'utf-8');
 
 const variables = {};
