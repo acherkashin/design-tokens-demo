fda6616c94e192d71676a09fc79ae09c2613ecb6
Cherkashin Alexander <cherkalexander@gmail.com>
2023-11-12 10:12:00 +0300

    Link functions

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -19,10 +19,11 @@ module.exports = {
   files: ['**/*.css'],
   rules: {
     'scale-unlimited/declaration-strict-value': [
-      ['color', 'background-color', 'font-size', 'font-weight', 'font-family', 'border-color'],
+      ['filter', 'color', 'background-color', 'font-size', 'font-weight', 'font-family', 'border-color'],
       {
         ignoreValues: ['currentColor', 'unset', 'inherit', 'initial', 'transparent'],
         autoFixFunc: autoFixFunc,
+        ignoreFunctions: false,
       },
     ],
   },
